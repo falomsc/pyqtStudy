3365c7653294f88369e1c71bc37306d11b9412a0
falomsc <falomsc@163.com>
2020-08-13 08:56:40 +0800

    update 08013_1

---
+++ ch08/my0806.py
@@ -0,0 +1,254 @@
+import sys
+import random
+
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import Qt, QDir, QSize, pyqtSignal, QPoint, pyqtSlot, QPointF
+from PyQt5.QtGui import QIcon, QKeyEvent, QBrush, QPolygonF, QPen
+from PyQt5.QtWidgets import QFrame, QFileSystemModel, QGraphicsView, QLabel, QGraphicsScene, QGraphicsItem, \
+    QColorDialog, QGraphicsRectItem, QGraphicsEllipseItem, QGraphicsPolygonItem, QGraphicsLineItem, QInputDialog, \
+    QGraphicsTextItem
+
+
+class Ui_MainWindow():
+    def setupUi(self, qMainWindow):
+        qMainWindow.resize(568, 380)
+        self.qWidget = QtWidgets.QWidget(qMainWindow)
+        self.qToolBar1 = QtWidgets.QToolBar(qMainWindow)
+        self.qToolBar1.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        self.qToolBar1.setIconSize(QSize(16, 16))
+        self.qToolBar2 = QtWidgets.QToolBar(qMainWindow)
+        self.qToolBar2.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        self.qToolBar2.setIconSize(QSize(16, 16))
+        self.qStatusBar = QtWidgets.QStatusBar(qMainWindow)
+        # self.qMenBar = QtWidgets.QMenuBar(qMainWindow)
+
+        self.qAction1 = QtWidgets.QAction(QIcon("../image/zoomin.png"), "放大", self.qToolBar1)
+        self.qAction2 = QtWidgets.QAction(QIcon("../image/zoomout.png"), "缩小", self.qToolBar1)
+        self.qAction3 = QtWidgets.QAction(QIcon("../image/420.bmp"), "恢复", self.qToolBar1)
+        self.qAction4 = QtWidgets.QAction(QIcon("../image/rotateleft.png"), "左旋转", self.qToolBar1)
+        self.qAction5 = QtWidgets.QAction(QIcon("../image/rotateright.png"), "右旋转", self.qToolBar1)
+        self.qAction6 = QtWidgets.QAction(QIcon("../image/528.bmp"), "前置", self.qToolBar1)
+        self.qAction7 = QtWidgets.QAction(QIcon("../image/526.bmp"), "后置", self.qToolBar1)
+        self.qAction8 = QtWidgets.QAction(QIcon("../image/UNGROUP.BMP"), "组合", self.qToolBar1)
+        self.qAction9 = QtWidgets.QAction(QIcon("../image/128_1.bmp"), "打散", self.qToolBar1)
+        self.qAction10 = QtWidgets.QAction(QIcon("../image/108.bmp"), "删除", self.qToolBar1)
+        self.qAction11 = QtWidgets.QAction(QIcon("../image/132.bmp"), "退出", self.qToolBar1)
+        self.qToolBar1.addAction(self.qAction1)
+        self.qToolBar1.addAction(self.qAction2)
+        self.qToolBar1.addAction(self.qAction3)
+        self.qToolBar1.addSeparator()
+        self.qToolBar1.addAction(self.qAction4)
+        self.qToolBar1.addAction(self.qAction5)
+        self.qToolBar1.addAction(self.qAction6)
+        self.qToolBar1.addAction(self.qAction7)
+        self.qToolBar1.addAction(self.qAction8)
+        self.qToolBar1.addAction(self.qAction9)
+        self.qToolBar1.addSeparator()
+        self.qToolBar1.addAction(self.qAction10)
+        self.qToolBar1.addSeparator()
+        self.qToolBar1.addAction(self.qAction11)
+
+
+        self.qAction12 = QtWidgets.QAction(QIcon("../image/RECTANGL.BMP"), "矩形", self.qToolBar2)
+        self.qAction13 = QtWidgets.QAction(QIcon("../image/ELLIPSE.BMP"), "椭圆", self.qToolBar2)
+        self.qAction14 = QtWidgets.QAction(QIcon("../image/08.JPG"), "圆形", self.qToolBar2)
+        self.qAction15 = QtWidgets.QAction(QIcon("../image/Icon1242.ico"), "三角形", self.qToolBar2)
+        self.qAction16 = QtWidgets.QAction(QIcon("../image/FREEFORM.BMP"), "梯形", self.qToolBar2)
+        self.qAction17 = QtWidgets.QAction(QIcon("../image/LINE.BMP"), "直线", self.qToolBar2)
+        self.qAction18 = QtWidgets.QAction(QIcon("../image/800.bmp"), "文字", self.qToolBar2)
+        self.qToolBar2.addAction(self.qAction12)
+        self.qToolBar2.addAction(self.qAction13)
+        self.qToolBar2.addAction(self.qAction14)
+        self.qToolBar2.addAction(self.qAction15)
+        self.qToolBar2.addAction(self.qAction16)
+        self.qToolBar2.addAction(self.qAction17)
+        self.qToolBar2.addAction(self.qAction18)
+
+        qMainWindow.addToolBar(Qt.TopToolBarArea, self.qToolBar1)
+        qMainWindow.addToolBar(Qt.LeftToolBarArea, self.qToolBar2)
+        qMainWindow.setCentralWidget(self.qWidget)
+
+        self.qAction1.setObjectName("qAction1")
+        self.qAction2.setObjectName("qAction2")
+        self.qAction3.setObjectName("qAction3")
+        self.qAction4.setObjectName("qAction4")
+        self.qAction5.setObjectName("qAction5")
+        self.qAction6.setObjectName("qAction6")
+        self.qAction7.setObjectName("qAction7")
+        self.qAction8.setObjectName("qAction8")
+        self.qAction9.setObjectName("qAction9")
+        self.qAction10.setObjectName("qAction10")
+        self.qAction11.setObjectName("qAction11")
+        self.qAction12.setObjectName("qAction12")
+        self.qAction13.setObjectName("qAction13")
+        self.qAction14.setObjectName("qAction14")
+        self.qAction15.setObjectName("qAction15")
+        self.qAction16.setObjectName("qAction16")
+        self.qAction17.setObjectName("qAction17")
+        self.qAction18.setObjectName("qAction18")
+
+        self.qAction11.triggered.connect(qMainWindow.close)
+        QtCore.QMetaObject.connectSlotsByName(qMainWindow)
+
+class QmyGraphicsView(QGraphicsView):
+    mouseMove = pyqtSignal(QPoint)
+    mouseClicked = pyqtSignal(QPoint)
+    mouseDoubleClick = pyqtSignal(QPoint)
+    keyPress = pyqtSignal(QKeyEvent)
+
+    def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
+        point = event.pos()
+        self.mouseMove.emit(point)
+        super().mousePressEvent(event)
+
+    def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
+        if event.button() == Qt.LeftButton:
+            point = event.pos()
+            self.mouseClicked.emit(point)
+        super().mousePressEvent(event)
+
+    def mouseDoubleClickEvent(self, event: QtGui.QMouseEvent) -> None:
+        if event.button() == Qt.LeftButton:
+            point = event.pos()
+            self.mouseDoubleClick.emit(point)
+        super().mouseDoubleClickEvent(event)
+
+    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
+        self.keyPress.emit(event)
+        super().keyPressEvent(event)
+
+
+
+class QmyMainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.__buildStatusBar()
+        self.__iniGraphicsSystem()
+
+        self.__ItemId = 1
+        self.__ItemDesc = 2
+        self.__seqNum = 0
+        self.__backZ = 0
+        self.__frontZ = 0
+
+    def __buildStatusBar(self):
+        self.__labViewCord = QLabel("View 坐标")
+        self.__labViewCord.setMinimumWidth(150)
+        self.ui.qStatusBar.addWidget(self.__labViewCord)
+
+        self.__labSceneCord = QLabel("Scene 坐标")
+        self.__labSceneCord.setMinimumWidth(150)
+        self.ui.qStatusBar.addWidget(self.__labSceneCord)
+
+        self.__labItemCord = QLabel("Item 坐标")
+        self.__labItemCord.setMinimumWidth(150)
+        self.ui.qStatusBar.addWidget(self.__labItemCord)
+
+        self.__labItemInfo = QLabel("ItemInfo: ")
+        self.ui.qStatusBar.addWidget(self.__labItemInfo)
+
+    def __iniGraphicsSystem(self):
+        self.view = QmyGraphicsView(self)
+        self.setCentralWidget(self.view)
+
+        self.scene = QGraphicsScene(-300, -200, 600, 200)
+        self.view.setScene(self.scene)
+        self.view.setMouseTracking(True)
+        self.view.setDragMode(QGraphicsView.RubberBandDrag)
+
+        self.view.mouseMove.connect(self.do_mouseMove)
+        self.view.mouseClicked.connect(self.do_mouseClicked)
+        self.view.mouseDoubleClick.connect(self.do_mouseDoubleClick)
+        self.view.keyPress.connect(self.do_keyPress)
+
+    def __setItemProperties(self, item, desc):
+        item.setFlag(QGraphicsItem.ItemIsFocusable)
+        item.setFlag(QGraphicsItem.ItemIsMovable)
+        item.setFlag(QGraphicsItem.ItemIsSelectable)
+
+        self.__fontZ += 1
+        item.setZValue(self.__fontZ)
+        item.setPos(-150+random.randint(1,200), -200+random.randint(1,200))
+
+        self.__seqNum += 1
+        item.setData(self.__ItemId, self.__seqNum)
+        item.setData(self.__ItemDesc, desc)
+
+        self.scene.addItem(item)
+        self.scene.cleafSelection()
+        item.setSelected(True)
+
+    def __setBrushColor(self, item):
+        color = item.brush().color()
+        color = QColorDialog.getColor(color, self, "选择填充颜色")
+        if color.isValid():
+            item.setBrush(QBrush(color))
+
+    @pyqtSlot()
+    def on_qAction12_triggered(self):
+        item = QGraphicsRectItem(-50, -25, 100, 50)
+        item.setBrush(QBrush(Qt.yellow))
+        self.__setItemProperties(item, "矩形")
+
+    @pyqtSlot()
+    def on_qAction13_triggered(self):
+        item = QGraphicsEllipseItem(-50, -30, 100, 60)
+        item.setBrush(QBrush(Qt.blue))
+        self.__setItemProperties(item, "椭圆")
+
+    @pyqtSlot()
+    def on_qAction14_triggered(self):
+        item = QGraphicsEllipseItem(-50, -50, 100, 100)
+        item.setBrush(QBrush(Qt.cyan))
+        self.__setItemProperties(item, "圆形")
+
+    @pyqtSlot()
+    def on_qAction15_triggered(self):
+        item = QGraphicsPolygonItem()
+        points = [QPointF(0, -40), QPointF(60, 40), QPointF(-60, 40)]
+        item.setPolygon(QPolygonF(points))
+        item.setBrush(QBrush(Qt.magenta))
+        self.__setItemProperties(item, "三角形")
+
+    @pyqtSlot()
+    def on_qAction16_triggered(self):
+        item = QGraphicsPolygonItem()
+        points = [QPointF(-40, -40), QPointF(40, -40), QPointF(100, 40), QPointF(-100, 40)]
+        item.setPolygon(QPolygonF(points))
+        item.setBrush(QBrush(Qt.green))
+        self.__setItemProperties(item, "梯形")
+
+    @pyqtSlot()
+    def on_qAction17_triggered(self):
+        item = QGraphicsLineItem(-100, 0, 100, 0)
+        pen = QPen(Qt.red)
+        pen.setWidth(4)
+        item.setPen(pen)
+        self.__setItemProperties(item, "直线")
+
+    @pyqtSlot()
+    def on_qAction18_triggered(self):
+        strText, OK = QInputDialog.getText(self, "输入", "请输入文字")
+        if(not OK):
+            return
+        item = QGraphicsTextItem(strText)
+        font = self.font()
+        font.setPointSize(20)
+        font.setBold(True)
+        item.setFont(font)
+        item.setDefaultTextColor(Qt.black)
+        self.__setItemProperties(item, "文字")
+
+
+
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    mw = QmyMainWindow()
+    mw.show()
+    sys.exit(app.exec_(), )
